33e488a2a28a959ce5d3a649c42326a83eec5a8a
AkiMadie16 <madie86@gmail.com>
2022-08-25 15:57:27 +0930

    tic-tac-toe update

--- tictactoe.js
+++ tictactoe.js
@@ -1,37 +1,46 @@
 
     
-    var winningCombos= [["box0", "box1", "box2"], ["box3", "box4", "box5"], ["box6", "box7", "box8"], ["box0", "box4", "box8"], ["box2", "box4", "box6"], ["box0", "box3", "box6"], ["box1", "box4","box7"],["box2", "box5", "box8"]];
+    var winningCombos= [["b0", "b1", "b2"], ["b3", "b4", "b5"], ["b6", "b7", "b8"], ["b0", "b4", "b8"], ["b2", "b4", "b6"], ["b0", "b3", "b6"], ["b1", "b4","b7"],["b2", "b5", "b8"]];
 
     
     var currentPlayer = 'X';
     const player1 = 'X';
     const player2 = 'O';
 
-    var board= document.querySelectorAll('.box');
-
-      for (let i = 0; i < board.length; i++) {
-       
-        board[i].addEventListener('click', function(event) { 
-        let box = event.target;
-        console.log(box.textContent)
-         if(i % 2 === 0) {
-           box.textContent = 'X';
-          }
-          else
-              box.textContent = 'O';
-      })
-    }
+    var board = document.querySelector('.board');
+    let turnCounter = 0;
+    board.addEventListener('click', function(event) { 
+    let box = event.target;
+    console.log(box.textContent)
+      if (box.tagName === "DIV") {
+        if(turnCounter % 2 === 0) {
+          box.textContent = 'O';
+        } else {
+          box.textContent = 'X';
+        }
+        turnCounter++
+      }
+  
      for (let j = 0; j < winningCombos.length; j++) { 
       
-    if (winningCombos[j].includes(player1) || winningCombos[j].includes(player1)) {
-      var winnerScore = document.querySelector('#boxTopBottom')
+     } if (board[winningCombos[j][0]].includes(player1) && board[winningCombos[j][1]].includes(player1) && board[winningCombos[j][2]].includes(player1)) {
+    //  var winnerScore = document.querySelector('#boxTopBottom')
 
-        winnerScore.textContent = 'currentPlayer';   
-      }
-      else
-        winnerScore.textContent = 'Draw';
+      // winnerScore.textContent = 'currentPlayer' + ' winner'; 
+      document.querySelector('#boxTopBottom').textContent = 'player1' + ' winner';
+
+     } else if (board[winningCombos[j][0]].includes(player1) && board[winningCombos[j][1]].includes(player1) && board[winningCombos[j][2]].includes(player1)) {
+      //  var winnerScore = document.querySelector('#boxTopBottom')
+  
+        // winnerScore.textContent = 'currentPlayer' + ' winner'; 
+        document.querySelector('#boxTopBottom').textContent = 'player2' + ' winner';
+      
      }
-
+    
+    } else { 
+      document.querySelector('#boxTopBottom').textContent = 'Draw';
+     }
+    })
 
 
 
